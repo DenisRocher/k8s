f92997b83f11e914fa5e68a5041984eb7b4a9299
Denis Rocher <denis.rocher@gmail.com>
2021-11-18 15:24:19 -0300

    Kubernetes NS - Limits - Requests

--- k8s-hands-on/backend/src/main.go
+++ k8s-hands-on/backend/src/main.go
@@ -29,7 +29,11 @@ func ServeHTTP(w http.ResponseWriter, r *http.Request) {
     }
 
     w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+
+    /*
     w.WriteHeader(http.StatusOK)
+    */
     /* resp := fmt.Sprintf("La hora es %v y hostname es %v", time.Now(), os.Getenv("HOSTNAME"))*/
     w.Write(jsonResp)
 }
